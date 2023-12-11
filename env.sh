71ac0f2dabc408d92cee0db59440c6f94a87754a
smart_monkey <rouven@propellerheads.xyz>
2023-12-11 10:18:12 +0100

    .env instead of env.sh

---
+++ env.sh
@@ -0,0 +1,2 @@
+export STREAMINGFAST_KEY=server_6e3a98b2aa01c779e44e2d9b80b337be
+export SUBSTREAMS_API_TOKEN=$(curl https://auth.streamingfast.io/v1/auth/issue -s --data-binary '{"api_key":"'$STREAMINGFAST_KEY'"}' | jq -r .token)
