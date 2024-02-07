b04085e257e60315f78ce90c990364a8a9cae519
Diana Carvalho <diana@datarevenue.com>
2024-02-07 15:56:58 -0500

    Handle failures on decimals or symbol when querying tokens
    Add abi as an attribute of TokenPreProcessor
    
    --- don't change below this line ---
    ENG-2740

--- tycho-indexer/src/extractor/evm/token_pre_processor.rs
+++ tycho-indexer/src/extractor/evm/token_pre_processor.rs
@@ -10,6 +10,7 @@ use std::{fs, sync::Arc};
 
 pub struct TokenPreProcessor {
     client: Arc<Provider<Http>>,
+    contract_abi: Abi,
 }
 
 impl TokenPreProcessor {
@@ -17,46 +18,45 @@ impl TokenPreProcessor {
         let client = Provider::<Http>::try_from(rpc_url)
             .expect("Error creating HTTP provider")
             .into();
-
-        TokenPreProcessor { client }
-    }
-
-    pub async fn get_tokens(
-        &self,
-        addresses: Vec<H160>,
-    ) -> Vec<Result<ERC20Token, Box<dyn std::error::Error>>> {
-        let mut tokens_info = Vec::new();
-
         let abi_str = fs::read_to_string("src/extractor/evm/abi/erc20.json")
             .expect("Unable to read ABI file");
-        let abi = from_str::<Abi>(&abi_str).expect("Unable to parse ABI");
+        let contract_abi = from_str::<Abi>(&abi_str).expect("Unable to parse ABI");
 
+        TokenPreProcessor { client, contract_abi }
+    }
+
+    pub async fn get_tokens(&self, addresses: Vec<H160>) -> Vec<ERC20Token> {
+        let mut tokens_info = Vec::new();
         for address in addresses {
-            let contract = Contract::new(address, abi.clone(), self.client.clone());
+            let contract = Contract::new(address, self.contract_abi.clone(), self.client.clone());
 
             let symbol: Result<String, _> = contract
                 .method("symbol", ())
-                .expect("Error preparing request")
+                .expect("Error preparing request for token's symbol")
                 .call()
                 .await;
 
             let decimals: Result<u8, _> = contract
                 .method("decimals", ())
-                .expect("Error preparing request")
+                .expect("Error preparing request for token's decimals")
                 .call()
                 .await;
 
-            match (symbol, decimals) {
-                (Ok(symbol), Ok(decimals)) => tokens_info.push(Ok(ERC20Token {
-                    address,
-                    symbol,
-                    decimals: decimals.into(),
-                    tax: 0,
-                    gas: vec![],
-                    chain: Chain::Ethereum,
-                })),
-                (Err(e), _) | (_, Err(e)) => println!("Error fetching token data: {:?}", e),
-            }
+            let (symbol, decimals, quality) = match (symbol, decimals) {
+                (Ok(symbol), Ok(decimals)) => (symbol, decimals, 100),
+                (Ok(symbol), Err(_)) => (symbol, 18, 0),
+                (Err(_), Ok(decimals)) => (address.to_string(), decimals, 0),
+                (Err(_), Err(_)) => (address.to_string(), 18, 0),
+            };
+            tokens_info.push(ERC20Token {
+                address,
+                symbol,
+                decimals: decimals.into(),
+                tax: 0,
+                gas: vec![],
+                chain: Chain::Ethereum,
+                quality,
+            });
         }
 
         tokens_info
@@ -76,15 +76,21 @@ mod tests {
 
         let weth_address: &str = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
         let usdc_address: &str = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-        let addresses =
-            vec![H160::from_str(weth_address).unwrap(), H160::from_str(usdc_address).unwrap()];
+        let fake_address: &str = "0xA0b86991c7456b36c1d19D4a2e9Eb0cE3606eB48";
+        let addresses = vec![
+            H160::from_str(weth_address).unwrap(),
+            H160::from_str(usdc_address).unwrap(),
+            H160::from_str(fake_address).unwrap(),
+        ];
 
         let results = processor.get_tokens(addresses).await;
-
-        assert_eq!(results.len(), 2);
-        assert_eq!(results[0].as_ref().unwrap().symbol, "WETH");
-        assert_eq!(results[0].as_ref().unwrap().decimals, 18);
-        assert_eq!(results[1].as_ref().unwrap().symbol, "USDC");
-        assert_eq!(results[1].as_ref().unwrap().decimals, 6);
+        println!("{:?}", results);
+        assert_eq!(results.len(), 3);
+        assert_eq!(results[0].symbol, "WETH");
+        assert_eq!(results[0].decimals, 18);
+        assert_eq!(results[1].symbol, "USDC");
+        assert_eq!(results[1].decimals, 6);
+        assert_eq!(results[2].symbol, "0xa0b8…eb48");
+        assert_eq!(results[2].decimals, 18);
     }
 }
