25c0ae6f4e1379c16b9d67f8fc44b5c5a594d612
zizou <111426680+flopell@users.noreply.github.com>
2025-04-22 12:58:29 +0200

    refactor: update entry point tracing schema and migration scripts
    This commit refactors our Postgres design: we're now linking protocol components to `entry_point_tracing_data` instead of `entry_point`. This is to keep track of what component is linked to what tracing data (previously lost during inserts).
    
    Also refactor the schema.patch, the patch data is now packed together at the top make it more robust to conflicts on new updates

--- tycho-storage/migrations/2025-04-09_dci_entry_points/up.sql
+++ tycho-storage/migrations/2025-04-09_dci_entry_points/up.sql
@@ -5,33 +5,41 @@ CREATE TYPE entry_point_tracing_type AS ENUM (
 
 CREATE TABLE IF NOT EXISTS "entry_point"(
     "id" bigserial PRIMARY KEY,
+    "external_id" text UNIQUE NOT NULL,
     "target" bytea NOT NULL,
     "signature" text NOT NULL,
+    "created_at" timestamp with time zone NOT NULL DEFAULT now(),
+    "updated_at" timestamp with time zone NOT NULL DEFAULT now(),
     UNIQUE ("target", "signature")
 );
 
 CREATE TABLE IF NOT EXISTS "entry_point_tracing_data"(
-    "entry_point_id" bigint REFERENCES "entry_point"(id) NOT NULL,
+    "id" bigserial PRIMARY KEY,
+    "entry_point_id" bigint REFERENCES "entry_point"(id) ON DELETE CASCADE NOT NULL,
     "tracing_type" entry_point_tracing_type NOT NULL,
     "data" JSONB,
-    PRIMARY KEY ("entry_point_id", "tracing_type", "data")
+    "created_at" timestamp with time zone NOT NULL DEFAULT now(),
+    "updated_at" timestamp with time zone NOT NULL DEFAULT now(),
+    UNIQUE ("entry_point_id", "tracing_type", "data")
 );
 
-CREATE TABLE IF NOT EXISTS "protocol_component_holds_entry_point"(
-    "protocol_component_id" bigint REFERENCES "protocol_component"(id) NOT NULL,
-    "entry_point_id" bigint REFERENCES "entry_point"(id) NOT NULL,
-    PRIMARY KEY ("protocol_component_id", "entry_point_id")
+CREATE TABLE IF NOT EXISTS "protocol_component_holds_entry_point_tracing_data"(
+    "protocol_component_id" bigint REFERENCES "protocol_component"(id) ON DELETE CASCADE NOT NULL,
+    "entry_point_tracing_data_id" bigint REFERENCES "entry_point_tracing_data"(id) ON DELETE CASCADE NOT NULL,
+    PRIMARY KEY ("protocol_component_id", "entry_point_tracing_data_id")
 );
 
-CREATE TABLE IF NOT EXISTS "traced_entry_point"(
-    "entry_point_id" bigint NOT NULL REFERENCES "entry_point"(id) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS "entry_point_tracing_result"(
+    "entry_point_tracing_data_id" bigint NOT NULL REFERENCES "entry_point_tracing_data"(id) ON DELETE CASCADE PRIMARY KEY,
     "detection_block" bigint NOT NULL REFERENCES "block"(id),
-    "detection_data" JSONB NOT NULL
+    "detection_data" JSONB NOT NULL,
+    "created_at" timestamp with time zone NOT NULL DEFAULT now(),
+    "updated_at" timestamp with time zone NOT NULL DEFAULT now()
 );
 
 -- Keep tracks of the m2m relation between entry_points and accounts
-CREATE TABLE IF NOT EXISTS "entry_point_calls_account"(
-    "entry_point_id" bigint NOT NULL REFERENCES "entry_point"(id),
-    "account_id" bigint NOT NULL REFERENCES "account"(id),
-    PRIMARY KEY("entry_point_id","account_id")
+CREATE TABLE IF NOT EXISTS "entry_point_tracing_data_calls_account"(
+    "entry_point_tracing_data_id" bigint NOT NULL REFERENCES "entry_point_tracing_data"(id) ON DELETE CASCADE,
+    "account_id" bigint NOT NULL REFERENCES "account"(id) ON DELETE CASCADE,
+    PRIMARY KEY("entry_point_tracing_data_id","account_id")
 )
