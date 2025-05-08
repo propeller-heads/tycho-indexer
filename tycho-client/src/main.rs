61eaeb236624fd61e0fe89a47fd75ec4ea183b1e
Louise Poole <louise@datarevenue.com>
2025-05-08 17:24:34 +0200

    refactor(tycho-client): improve cli main function

--- tycho-client/src/main.rs
+++ tycho-client/src/main.rs
@@ -2,8 +2,8 @@ use tycho_client::cli::run_cli;
 
 #[tokio::main]
 async fn main() {
-    if let Err(e) = run_cli().await {
+    run_cli().await.unwrap_or_else(|e| {
         eprintln!("Error: {e}");
         std::process::exit(1);
-    }
+    });
 }
