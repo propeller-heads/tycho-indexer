5edf7e8c6b9bf6538a8f205db24ad5d096861862
Louise Poole <louise@datarevenue.com>
2025-05-23 14:28:00 +0200

    feat: add dci plugin to ProtocolExtractor

--- tycho-indexer/src/extractor/runner.rs
+++ tycho-indexer/src/extractor/runner.rs
@@ -21,12 +21,17 @@ use tycho_common::{
     models::{Chain, ExtractorIdentity, FinancialType, ImplementationType, ProtocolType},
     Bytes,
 };
-use tycho_ethereum::token_pre_processor::EthereumTokenPreProcessor;
+use tycho_ethereum::{
+    account_extractor::contract::EVMBatchAccountExtractor,
+    entrypoint_tracer::tracer::EVMEntrypointService,
+    token_pre_processor::EthereumTokenPreProcessor,
+};
 use tycho_storage::postgres::cache::CachedGateway;
 
 use crate::{
     extractor::{
         chain_state::ChainState,
+        dynamic_contract_indexer::DynamicContractIndexer,
         post_processors::POST_PROCESSOR_REGISTRY,
         protocol_cache::ProtocolMemoryCache,
         protocol_extractor::{ExtractorPgGateway, ProtocolExtractor},
@@ -105,7 +110,7 @@ impl MessageSender for ExtractorHandle {
 type SubscriptionsMap = HashMap<u64, Sender<ExtractorMsg>>;
 
 pub struct ExtractorRunner {
-    extractor: Arc<dyn Extractor>,
+    extractor: Arc<Mutex<dyn Extractor>>,
     substreams: SubstreamsStream,
     subscriptions: Arc<Mutex<SubscriptionsMap>>,
     next_subscriber_id: u64,
@@ -117,7 +122,7 @@ pub struct ExtractorRunner {
 
 impl ExtractorRunner {
     pub fn new(
-        extractor: Arc<dyn Extractor>,
+        extractor: Arc<Mutex<dyn Extractor>>,
         substreams: SubstreamsStream,
         subscriptions: Arc<Mutex<SubscriptionsMap>>,
         control_rx: Receiver<ControlMessage>,
@@ -140,7 +145,7 @@ impl ExtractorRunner {
             .unwrap_or_else(|| tokio::runtime::Handle::current());
 
         runtime.spawn(async move {
-            let id = self.extractor.get_id();
+            let id = self.extractor.lock().await.get_id();
             loop {
                 // this is the main info span of an extractor
                 let loop_span = tracing::info_span!(
@@ -185,7 +190,7 @@ impl ExtractorRunner {
                                     let start_time = std::time::Instant::now();
 
                                     // TODO: change interface to take a reference to avoid this clone
-                                    match self.extractor.handle_tick_scoped_data(data.clone()).await {
+                                    match self.extractor.lock().await.handle_tick_scoped_data(data.clone()).await {
                                         Ok(Some(msg)) => {
                                             trace!("Propagating new block data message.");
                                             Self::propagate_msg(&self.subscriptions, msg).await
@@ -209,7 +214,7 @@ impl ExtractorRunner {
                                 }
                                 Some(Ok(BlockResponse::Undo(undo_signal))) => {
                                     info!(block=?&undo_signal.last_valid_block,  "Revert requested!");
-                                    match self.extractor.handle_revert(undo_signal.clone()).await {
+                                    match self.extractor.lock().await.handle_revert(undo_signal.clone()).await {
                                         Ok(Some(msg)) => {
                                             trace!("Propagating block undo message.");
                                             Self::propagate_msg(&self.subscriptions, msg).await
@@ -356,16 +361,22 @@ impl ExtractorConfig {
     }
 }
 
+pub enum DCIType {
+    /// RPC DCI plugin - uses the RPC endpoint to fetch the account data
+    RPC(String),
+}
+
 pub struct ExtractorBuilder {
     config: ExtractorConfig,
     endpoint_url: String,
     s3_bucket: Option<String>,
     token: String,
-    extractor: Option<Arc<dyn Extractor>>,
+    extractor: Option<Arc<Mutex<dyn Extractor>>>,
     final_block_only: bool,
     /// Handle of the tokio runtime on which the extraction tasks will be run.
     /// If 'None' the default runtime will be used.
     runtime_handle: Option<Handle>,
+    dci_type: Option<DCIType>,
 }
 
 pub type HandleResult = (JoinHandle<Result<(), ExtractionError>>, ExtractorHandle);
@@ -380,9 +391,11 @@ impl ExtractorBuilder {
             extractor: None,
             final_block_only: false,
             runtime_handle: None,
+            dci_type: None,
         }
     }
 
+    /// Set the substreams endpoint url
     pub fn endpoint_url(mut self, val: &str) -> Self {
         val.clone_into(&mut self.endpoint_url);
         self
@@ -413,8 +426,13 @@ impl ExtractorBuilder {
         self
     }
 
+    pub fn set_dci_plugin(mut self, dci_type: DCIType) -> Self {
+        self.dci_type = Some(dci_type);
+        self
+    }
+
     #[cfg(test)]
-    pub fn set_extractor(mut self, val: Arc<dyn Extractor>) -> Self {
+    pub fn set_extractor(mut self, val: Arc<Mutex<dyn Extractor>>) -> Self {
         self.extractor = Some(val);
         self
     }
@@ -490,8 +508,45 @@ impl ExtractorBuilder {
             })
             .transpose()?;
 
-        self.extractor = Some(Arc::new(
-            ProtocolExtractor::new(
+        let dci_plugin = if let Some(ref dci_type) = self.dci_type {
+            Some(match dci_type {
+                DCIType::RPC(rpc_url) => {
+                    let account_extractor =
+                        EVMBatchAccountExtractor::new(rpc_url, self.config.chain)
+                            .await
+                            .map_err(|err| {
+                                ExtractionError::Setup(format!(
+                                    "Failed to create account extractor for {rpc_url}: {err}"
+                                ))
+                            })?;
+                    let tracer = EVMEntrypointService::try_from_url(rpc_url).map_err(|err| {
+                        ExtractionError::Setup(format!(
+                            "Failed to create entrypoint tracer for {rpc_url}: {err}"
+                        ))
+                    })?;
+                    DynamicContractIndexer::new(
+                        self.config.chain,
+                        self.config.name.clone(),
+                        cached_gw.clone(),
+                        account_extractor,
+                        tracer,
+                    )
+                }
+            })
+        } else {
+            None
+        };
+
+        self.extractor = Some(Arc::new(Mutex::new(
+            ProtocolExtractor::<
+                ExtractorPgGateway,
+                EthereumTokenPreProcessor,
+                DynamicContractIndexer<
+                    EVMBatchAccountExtractor,
+                    EVMEntrypointService,
+                    CachedGateway,
+                >,
+            >::new(
                 gw,
                 &self.config.name,
                 self.config.chain,
@@ -501,9 +556,10 @@ impl ExtractorBuilder {
                 protocol_types,
                 token_pre_processor.clone(),
                 post_processor,
+                dci_plugin,
             )
             .await?,
-        ));
+        )));
 
         Ok(self)
     }
@@ -514,8 +570,9 @@ impl ExtractorBuilder {
             .extractor
             .clone()
             .expect("Extractor not set");
+        let extractor_id = extractor.lock().await.get_id();
 
-        tracing::Span::current().record("id", format!("{}", extractor.get_id()));
+        tracing::Span::current().record("id", format!("{extractor_id}"));
 
         self.ensure_spkg().await?;
 
@@ -531,7 +588,11 @@ impl ExtractorBuilder {
                 .map_err(|err| ExtractionError::SubstreamsError(err.to_string()))?,
         );
 
-        let cursor = extractor.get_cursor().await;
+        let cursor = extractor
+            .lock()
+            .await
+            .get_cursor()
+            .await;
         let stream = SubstreamsStream::new(
             endpoint,
             Some(cursor),
@@ -540,10 +601,9 @@ impl ExtractorBuilder {
             self.config.start_block,
             self.config.stop_block.unwrap_or(0) as u64,
             self.final_block_only,
-            extractor.get_id().to_string(),
+            extractor_id.to_string(),
         );
 
-        let id = extractor.get_id();
         let (ctrl_tx, ctrl_rx) = mpsc::channel(128);
         let runner = ExtractorRunner::new(
             extractor,
@@ -554,7 +614,7 @@ impl ExtractorBuilder {
         );
 
         let handle = runner.run();
-        Ok((handle, ExtractorHandle::new(id, ctrl_tx)))
+        Ok((handle, ExtractorHandle::new(extractor_id, ctrl_tx)))
     }
 }
 
@@ -640,7 +700,7 @@ mod test {
             .returning(ExtractorIdentity::default);
 
         // Build the ExtractorRunnerBuilder
-        let extractor = Arc::new(mock_extractor);
+        let extractor = Arc::new(Mutex::new(mock_extractor));
         let builder = ExtractorBuilder::new(
             &ExtractorConfig::new(
                 "test_module".to_owned(),
