58e5a5a1fe659347fda988a6a0a5f10fbe698e04
Louise Poole <louise@datarevenue.com>
2025-05-30 11:40:26 +0200

    refactor: update dci revert fn to take a block hash

--- tycho-indexer/src/extractor/protocol_extractor.rs
+++ tycho-indexer/src/extractor/protocol_extractor.rs
@@ -836,7 +836,7 @@ where
             dci_plugin
                 .lock()
                 .await
-                .process_revert(block_ref.number)
+                .process_revert(&block_hash)
                 .await?;
         }
 
