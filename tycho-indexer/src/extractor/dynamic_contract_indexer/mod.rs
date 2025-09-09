385d644dc2916e5cf8624478dd487c4b5f9db351
zizou <111426680+flopell@users.noreply.github.com>
2025-09-09 16:03:53 +0200

    feat: add component pausing in `hook_dci`
    This commit adds the logic to pause protocol components when anything goes wrong during hook dci process.

--- tycho-indexer/src/extractor/dynamic_contract_indexer/mod.rs
+++ tycho-indexer/src/extractor/dynamic_contract_indexer/mod.rs
@@ -9,3 +9,20 @@ mod hook_permissions_detector;
 pub(crate) mod hooks_dci_setup;
 mod metadata_orchestrator;
 mod rpc_metadata_provider;
+
+enum PausingReason {
+    #[allow(dead_code)]
+    Substreams,
+    TracingError,
+    MetadataError,
+}
+
+impl PausingReason {
+    fn get_reason_index(&self) -> u8 {
+        match self {
+            PausingReason::Substreams => 1,
+            PausingReason::TracingError => 2,
+            PausingReason::MetadataError => 3,
+        }
+    }
+}
