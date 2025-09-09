b138ec371faee1095cb65dd8f86d397aa073a871
zizou <111426680+flopell@users.noreply.github.com>
2025-09-09 18:24:33 +0200

    refactor: code clean and better error handling.
    This commit slightly improve code in hook_dci. Also it removes the hack with the transaction from the previous commit and return an error if somethings unexpected happens instead.

--- tycho-indexer/src/extractor/dynamic_contract_indexer/mod.rs
+++ tycho-indexer/src/extractor/dynamic_contract_indexer/mod.rs
@@ -17,12 +17,22 @@ enum PausingReason {
     MetadataError,
 }
 
-impl PausingReason {
-    fn get_reason_index(&self) -> u8 {
-        match self {
+impl From<PausingReason> for u8 {
+    fn from(reason: PausingReason) -> Self {
+        match reason {
             PausingReason::Substreams => 1,
             PausingReason::TracingError => 2,
             PausingReason::MetadataError => 3,
+        }
+    }
+}
+
+impl From<PausingReason> for tycho_common::Bytes {
+    fn from(reason: PausingReason) -> Self {
+        match reason {
+            PausingReason::Substreams => vec![1_u8].into(),
+            PausingReason::TracingError => vec![2_u8].into(),
+            PausingReason::MetadataError => vec![3_u8].into(),
         }
     }
 }
