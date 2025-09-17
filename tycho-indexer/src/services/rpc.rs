fa90d34a69579b588abf44991356a998e5965410
zizou <111426680+flopell@users.noreply.github.com>
2025-09-17 13:11:58 +0200

    chore: remove spammy logs

--- tycho-indexer/src/services/rpc.rs
+++ tycho-indexer/src/services/rpc.rs
@@ -940,8 +940,8 @@ where
                         ));
                     } else {
                         warn!(
-                            ?entry_point_id,
-                            ?tracing_param,
+                            %entry_point_id,
+                            %tracing_param,
                             "No tracing results found for entry point with params."
                         );
                     }
