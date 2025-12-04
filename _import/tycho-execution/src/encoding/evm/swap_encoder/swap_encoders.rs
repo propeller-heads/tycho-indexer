3a9a2a76851a09b05ff58a444706c6fcaaefd040
dianacarvalho1 <diana@propellerheads.xyz>
2025-12-04 16:39:52 +0000

    Update src/encoding/evm/swap_encoder/swap_encoders.rs
    Co-authored-by: Artem <48178500+ElusAegis@users.noreply.github.com>

--- _import/tycho-execution/src/encoding/evm/swap_encoder/swap_encoders.rs
+++ _import/tycho-execution/src/encoding/evm/swap_encoder/swap_encoders.rs
@@ -2165,7 +2165,7 @@ mod tests {
             let token_out = Bytes::from("0x4c9EDD5852cd905f086C759E8383e09bff1E68B3");
             let swap = SwapBuilder::new(curve_pool, token_in.clone(), token_out.clone()).build();
             let encoding_context = EncodingContext {
-                // The receiver was generated with `makeAddr("bob") using forge`
+                // The receiver was generated with `makeAddr("bob*") using forge`
                 receiver: Bytes::from("0x9964bff29baa37b47604f3f3f51f3b3c5149d6de"),
                 exact_out: false,
                 router_address: None,
