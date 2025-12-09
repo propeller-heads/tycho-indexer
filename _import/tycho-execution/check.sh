dd2191c05322752155a9bbb87792dffb1020f5eb
Artem <48178500+ElusAegis@users.noreply.github.com>
2025-12-09 15:38:21 +0000

    chore: add forge test command to check script

--- _import/tycho-execution/check.sh
+++ _import/tycho-execution/check.sh
@@ -4,3 +4,4 @@ cargo +nightly fmt --all --check
 cargo clippy --workspace --lib --all-targets --all-features -- -D clippy::dbg-macro
 cargo nextest run --workspace --lib --all-targets
 (cd foundry && forge fmt --check)
+(cd foundry && forge test)
