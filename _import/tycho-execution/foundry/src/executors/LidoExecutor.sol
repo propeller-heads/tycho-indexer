08f850bcc66932d1a395f29e2e453776f23fd02c
Ksenia Troshchenkova <ksenia@propellerheads.xyz>
2025-12-15 23:19:05 +0100

    refactor: cleanup

--- _import/tycho-execution/foundry/src/executors/LidoExecutor.sol
+++ _import/tycho-execution/foundry/src/executors/LidoExecutor.sol
@@ -18,7 +18,6 @@ interface LidoPool {
 }
 
 interface LidoWrappedPool {
-    // slither-disable-next-line naming-convention
     function wrap(uint256 _stETHAmount) external returns (uint256);
 
     function unwrap(uint256 _wstETHAmount) external returns (uint256);
