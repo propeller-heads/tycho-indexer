53ca64dd21ca665549360aaacec20e83aed2ee47
TAMARA LIPOWSKI <data.lipowski@extaccount.com>
2026-01-13 14:51:19 -0500

    feat(Maverick): Add tokenOut to executor
    - This will be useful for updating the delta accounting at a later stage]

--- _import/tycho-execution/foundry/test/protocols/MaverickV2.t.sol
+++ _import/tycho-execution/foundry/test/protocols/MaverickV2.t.sol
@@ -17,6 +17,7 @@ contract MaverickV2ExecutorExposed is MaverickV2Executor {
         returns (
             IERC20 tokenIn,
             address target,
+            address tokenOut,
             address receiver,
             RestrictTransferFrom.TransferType transferType
         )
@@ -43,6 +44,7 @@ contract MaverickV2ExecutorTest is TestUtils, Constants {
         bytes memory params = abi.encodePacked(
             GHO_ADDR,
             GHO_USDC_POOL,
+            USDC_ADDR,
             address(2),
             RestrictTransferFrom.TransferType.Transfer
         );
@@ -50,12 +52,14 @@ contract MaverickV2ExecutorTest is TestUtils, Constants {
         (
             IERC20 tokenIn,
             address target,
+            address tokenOut,
             address receiver,
             RestrictTransferFrom.TransferType transferType
         ) = maverickV2Exposed.decodeParams(params);
 
         assertEq(address(tokenIn), GHO_ADDR);
         assertEq(target, GHO_USDC_POOL);
+        assertEq(tokenOut, USDC_ADDR);
         assertEq(receiver, address(2));
         assertEq(
             uint8(transferType),
@@ -76,6 +80,7 @@ contract MaverickV2ExecutorTest is TestUtils, Constants {
         bytes memory protocolData = abi.encodePacked(
             GHO_ADDR,
             GHO_USDC_POOL,
+            USDC_ADDR,
             BOB,
             RestrictTransferFrom.TransferType.Transfer
         );
@@ -98,12 +103,14 @@ contract MaverickV2ExecutorTest is TestUtils, Constants {
         (
             IERC20 tokenIn,
             address pool,
+            address tokenOut,
             address receiver,
             RestrictTransferFrom.TransferType transferType
         ) = maverickV2Exposed.decodeParams(protocolData);
 
         assertEq(address(tokenIn), GHO_ADDR);
         assertEq(pool, GHO_USDC_POOL);
+        assertEq(tokenOut, USDC_ADDR);
         assertEq(receiver, BOB);
         assertEq(
             uint8(transferType),
